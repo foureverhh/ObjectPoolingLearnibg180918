87e391a5d573901d45148d298bc67d09630926b4
foureverhh <foureverhh@gmail.com>
2018-09-18 10:22:11 +0200

    Pooling works on the first step

---
+++ ObjectPoolingLearning/Assets/Scripts/CannonBallController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallController : MonoBehaviour {
+
+    private void OnBecameInvisible()
+    {
+        //Destroy(gameObject);
+        gameObject.SetActive(false);
+    }
+}
--- ObjectPoolingLearning/Assets/Scripts/CannonController.cs
+++ ObjectPoolingLearning/Assets/Scripts/CannonController.cs
@@ -8,8 +8,18 @@ public class CannonController : MonoBehaviour {
     public float delay = 0.3f;
     public float speed = 5f;
 
-	// Use this for initialization
-	void Start () {
+    public List<GameObject> cannonBallPool;
+    public int poolSize = 20;
+
+    // Use this for initialization
+    void Start () {
+        cannonBallPool = new List<GameObject>();
+        for(int i= 0; i < poolSize; i++)
+        {
+            GameObject cannonBall = Instantiate(cannonBallPrefab);
+            cannonBall.SetActive(false);
+            cannonBallPool.Add(cannonBall);
+        }
         StartCoroutine(ShootCannon());
 	}
 	
@@ -17,10 +27,26 @@ public class CannonController : MonoBehaviour {
     {
         while (true)
         {
-            GameObject cannonBall = Instantiate(cannonBallPrefab);
-            cannonBall.transform.position = gameObject.transform.position;
-            cannonBallPrefab.GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed);
+            GameObject cannonBall = GetCannonBall();//Instantiate(cannonBallPrefab);
+            if(cannonBall != null)
+            {
+                cannonBall.SetActive(true);
+                cannonBall.transform.position = gameObject.transform.position;
+                cannonBall.GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed);
+            }
             yield return new WaitForSeconds(delay); 
         }  
+    }
+
+    GameObject GetCannonBall()
+    {
+        for(int i = 0; i < cannonBallPool.Count; i++)
+        { 
+            //if the ball is not visible retrieve it
+            if (!cannonBallPool[i].activeInHierarchy){
+                return cannonBallPool[i];
+            }
+        }
+        return null;
     }
 }
